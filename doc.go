a2cc8b7e9a4d43847c016f84245c2240a18b2d30
Russ Cox <rsc@golang.org>
2015-02-23 19:57:00 -0500

    grind: expand docs

--- doc.go
+++ doc.go
@@ -22,5 +22,54 @@ Instead grind prints the differences a rewrite would introduce.
 Grind does not make backup copies of the files that it edits.
 Instead, use a version control system's ``diff'' functionality to inspect
 the changes that grind makes before committing them.
+
+Grind is a work in progress. More rewrites are planned.
+The initial use case for grind is cleaning up Go code that looks
+like C code.
+
+Dead Code Elimination
+
+Grind removes unreachable (dead) code. Code is considered
+unreachable if it is preceded by a terminating statement
+(see golang.org/ref/spec#Terminating_statements) and
+is not the target of a goto statement.
+
+Limitation: The analysis treats all block, if, for, switch, and
+select statements as non-terminating. In the future it may
+implement the more refined definitions given in the Go spec.
+
+Var Declaration Placement
+
+Grind moves var declarations as close as possible to their uses.
+When possible, it combines the declaration with the initialization,
+and it splits disjoint uses of a single variable into multiple variables.
+
+For example, consider:
+
+	var i int
+
+	...
+
+	for i = 0; i < 10; i++ {
+		use(i)
+	}
+
+	...
+
+	for i = 0; i < 10; i++ {
+		otherUse(i)
+	}
+
+Grind deletes the declaration and rewrites both loop initializers
+to use a combined declaration and assignment (i := 0).
+
+Limitation: Grind does not move variable declarations into loop bodies.
+It may in the future, provided it can determine that the variable
+is dead on entry to the body and that the variable does not
+escape (causing heap allocation inside the loop).
+
+Limitation: Grind considers variables that appear in closures off limits.
+A more sophisticated analysis might consider them in the future.
+
 */
 package main
